79c666f4acf2871d5770bf7ee9075f59b0c84f14
ANDRE FEITOSA MAIA <andrefmaia@hotmail.com>
2024-03-11 21:39:56 -0300

    React - 3PetShop
    3PetShop App

---
+++ Sql Scripts/SQL-ESTACIO TESTE.sql
@@ -0,0 +1,9 @@
+INSERT INTO teste (
+	cod, nome, data_nascimento)
+    VALUES 
+	(2,'Leandro Carlos', '19-08-1989'),
+    (3,'Fabio Henrique', '15-02-1989'),
+    (4,'Ana Pinheiro', '21-10-1899');
+	
+
+select * from teste
