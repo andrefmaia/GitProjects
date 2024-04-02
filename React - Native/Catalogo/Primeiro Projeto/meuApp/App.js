882c6d71b5e01ad4cb1152359555628104b7b387
ANDRE FEITOSA MAIA <andrefmaia@hotmail.com>
2024-04-02 15:28:27 -0300

    Cronometro APP
    CronometroApp Add with Scrowview

--- React - Native/Catalogo/Primeiro Projeto/meuApp/App.js
+++ React - Native/Catalogo/Primeiro Projeto/meuApp/App.js
@@ -1,58 +1,32 @@
 /*----------1º IMPORTAR O REACT E OS PACOTES DO REACT e useState--------------*/
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Image, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, ScrollView} from 'react-native';
 
 
-function App(){
 
-  const [img, setImg] = useState(require('./src/biscoito.png'));
-  const [textoFrase,setTextFrase] = useState('')
-
-  let frases = [
-      'E conhecereis a verdade, e a verdade vos libertará. João 8:32 ',
-      'Ainda que eu ande pelo vale da sombra da morte, não temerei mal algum, porque tu estás comigo; a tua vara e o teu cajado me consolam. Salmos 23:4',
-      'Clamarei ao Deus altíssimo, ao Deus que por mim tudo faz. Salmos 57: 2',
-      'Provérbios 9:10 diz: “O temor de Senhor é o princípio da sabedoria”. ',
-      'Porque para Deus nada será impossível. Lucas 1:37',
-      'Não ameis o mundo, nem o que há no mundo. Se alguém ama o mundo, o amor do Pai não está nele. 1 João 2:15',
-      'Disse-lhe Jesus: Porque me viste, creste? Bem-aventurados os que não viram e creram. João 20:29 '
-  ];
-
-  function quebraBiscoito(){
-    let numeroAleatorio = Math.floor(Math.random() * frases.length)
-
-    setTextFrase('"'+frases[numeroAleatorio]+'"');
-    setImg(require('./src/biscoitoAberto.png'));
-  };
-
-  function btnReiniciar(){
-    setTextFrase('');
-    setImg(require('./src/biscoito.png'));
-  }
+export default function App(){
  
+  
   return(
-   <View style={styles.container}>
-    
-      <Image
-      /*----------ACESSANDO A IMAGEM DENTRO DA PASTA DO PROJETO--------------*/
-        source={img}
-        style = {styles.img}
-      />
 
-      <Text style= {styles.textofrase}> {textoFrase}</Text>
+    /*<ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>*/
+    <View style={styles.container}>
+     
+      
+      
+      <ScrollView>
+          <View style={styles.box1}></View>
+          <View style={styles.box2}></View>
+          <View style={styles.box3}></View>
+          <View style={styles.box4}></View>
+          <View style={styles.box2}></View>
 
-      <TouchableOpacity style={styles.botao}   onPress={ quebraBiscoito }>
-        <View style = {styles.btnArea}>
-          <Text style ={styles.btnTexto} >Quebrar biscoito</Text>
-        </View>
-      </TouchableOpacity>
 
-      <TouchableOpacity style={[styles.botao, {marginTop: 15, borderColor: '#121212'}]}   onPress={btnReiniciar}>
-          <View style = {styles.btnArea}>
-            <Text style ={[styles.btnTexto, {color: '#121212'}  ]} >Reiniciar Biscoito</Text>
-          </View>
-        </TouchableOpacity>
-   </View>
+      </ScrollView>
+
+
+
+    </View>
 
   )
 }
@@ -63,44 +37,33 @@ const styles = StyleSheet.create({
 container:{
   flex: 1,
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+  backgroundColor: '#FFF'
 },
 
-img: {
-  width: 180,
-  height: 180,
+box1: {
+  height: 250,
+  backgroundColor: 'red'
 },
 
-textofrase: {
-  fontSize: 20,
-  color: '#dd7b22',
-  margin: 30,
-  fontStyle: 'italic',
-  textAlign: 'center'
+box2: {
+  height: 250,
+  backgroundColor: 'green'
 },
 
-botao: {
-  width: 230,
-  height: 50,
-  borderColor: '#dd7b22',
-  borderWidth: 2,
-  borderRadius: 25
+box3: {
+  height: 250,
+  backgroundColor: 'yellow'
 },
 
-btnArea: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center'
+box4: {
+ 
+  height: 250,
+  backgroundColor: 'blue'
 },
-
-btnTexto: {
-  fontSize: 17,
-  fontWeight: 'bold',
-  color: '#dd7b22'
-}
 
   
 })
 
-export default App;
+//export default App;
 
