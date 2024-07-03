c5b898ae4cb8eb55b431dcdcd4f2fdc947832912
ANDRE FEITOSA MAIA <andrefmaia@hotmail.com>
2024-07-03 17:44:59 -0300

    Update
    payfood

--- React/payfood/src/App.js
+++ React/payfood/src/App.js
@@ -1,71 +1,15 @@
-import Nome from "./Components/Nome";
+
 import { useState, useEffect } from "react";
 
 function App() {
-const [input, setInput] = useState('')
-const [tarefas, setTarefas] = useState([
-    'Pagar a conta Nubank',
-    'Estudar React'  
-]);
-
-
-useEffect( ()=>{
-  const tarefasStorage = localStorage.getItem('@tarefa');
-
-  if (tarefasStorage){
-    setTarefas(JSON.parse(tarefasStorage))
-  }
-
-  return () => {
-    //desmontar
-  }
-
-}, []);
-
-
-
-useEffect(() =>{
-  localStorage.setItem('@tarefa', JSON.stringify(tarefas) )
-}, [tarefas]);
-
-
-
-function handleRegister(e){
-  e.preventDefault();
-
-  setTarefas([...tarefas, input]);
-  setInput('');
-};
 
   return (
     <div>
-      <form onSubmit={ handleRegister}>
-        <label>Nome da Tarefa:</label><br/>
-        <input 
-        placeholder="Digite uma tarefa" 
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-       
-        /><br/><br/>
-
-          <button type="submit">Registrar</button>
-      </form>
+      <h1>Seja bem vindo!</h1>
       
-      <br/><br/>
+        
+    </div>
 
-      <ul>
-          {tarefas.map( tarefa => (
-          <li key={tarefa}>{tarefa}</li>
-          ))}
-      </ul>
-        
-        
-      </div>
-        
-        
-
-    
-   
   );
 }
 
